ad3de2165bbd23084f8363671ae20a796f6e3052
aalish <36036921+aalish@users.noreply.github.com>
2021-10-05 18:21:24 +0545

    Add files via upload

--- command_From_twitter.py
+++ command_From_twitter.py
@@ -19,39 +19,39 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
-updated_status= False
 def get_tweets(username):
-
-    global updated_status
-
-
-    number_of_tweets = 1
+    try:
+        global updated_status
 
 
-    for tweet in tweepy.Cursor(api.user_timeline, screen_name = username).items(number_of_tweets):
-        if (updated_status == False) :
-            tweet_status = "Device woke at " + str(datetime.datetime.now())
-            status = api.update_status(status=tweet_status)
+        number_of_tweets = 1
 
-            updated_status = True
 
-        tweet = str(tweet)
-
-        tweetdata = re.search("'text': '(.*)', 'truncated'", tweet)
-        tweetdata = tweetdata.group(1)
-        if (tweetdata.find("READY FOR NEXT COMMAND") == -1):
-            os.system(tweetdata)
-            try:
-                tweet_status = "SUCESSFULLY COMPLETED COMMAND: "+ tweetdata + "\nREADY FOR NEXT COMMAND"
+        for tweet in tweepy.Cursor(api.user_timeline, screen_name = username).items(number_of_tweets):
+            if (updated_status == False) :
+                tweet_status = "Device woke at " + str(datetime.datetime.now())
                 status = api.update_status(status=tweet_status)
-            except:
-                tweet_status = "SUCESSFULLY COMPLETED COMMAND: "+ tweetdata + "\nREADY FOR NEXT COMMAND " + str(random.randint(1,99999))
-                status = api.update_status(status=tweet_status)               
-            time.sleep(120)
-        else:
-            time.sleep(10)
-    
 
+                updated_status = True
+
+            tweet = str(tweet)
+            print("rotated")
+            tweetdata = re.search("'text': '(.*)', 'truncated'", tweet)
+            tweetdata = tweetdata.group(1)
+            if (tweetdata.find("READY FOR NEXT COMMAND") == -1):
+                os.system(tweetdata)
+                try:
+                    tweet_status = "SUCESSFULLY COMPLETED COMMAND: "+ tweetdata + "\nREADY FOR NEXT COMMAND"
+                    status = api.update_status(status=tweet_status)
+                except:
+                    tweet_status = "SUCESSFULLY COMPLETED COMMAND: "+ tweetdata + "\nREADY FOR NEXT COMMAND " + str(random.randint(1,99999))
+                    status = api.update_status(status=tweet_status)               
+
+            else:
+                time.sleep(10)
+    except:
+        pass
+    
 
 
 while(1):
